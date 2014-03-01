ecb9affd919be99b94bf8d1cb3d3733873684449
davidramos015 <davidramos015@gmail.com>
2014-03-01 15:05:18 -0600

    plantilla del home

---
+++ nFocusSitioWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace nFocusSitioWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
